e973b5941b8f08013e15b1e488c80df4932b0a14
aekkaphonz <boxxing34@gmail.com>
2025-01-15 11:38:57 +0700

    add func content service

--- src/content/content.service.ts
+++ src/content/content.service.ts
@@ -116,4 +116,19 @@ export class ContentService {
 
     return savedContent;
   }
+
+  async findById(id: string): Promise<Content> {
+
+    const isValidId = Types.ObjectId.isValid(id);
+    if (!isValidId) {
+      throw new BadRequestException('Please provide a valid ID.');
+    }
+    const content = await this.contentModel.findById(id).exec();
+    if (!content) {
+      throw new NotFoundException('Content not found');
+    }
+  
+    return content;
+  }
+  
 }
