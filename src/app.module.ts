b3d9ef889d1299fc3fd788078ad886f1affc735e
Tie0099 <guttipoom.456@gmail.com>
2025-01-15 09:27:28 +0700

    app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ContentModule } from './content/content.module';
-<<<<<<< HEAD
-import { PostsModule } from './posts/posts.module';
-=======
 import { CommentModule } from './comment/comment.module';
->>>>>>> 8c9309bff53e00f38c7bc1e80f84ccbb19eef52f
 
 @Module({
   imports: [
@@ -28,11 +24,7 @@ import { CommentModule } from './comment/comment.module';
     AuthModule,
     UserModule,
     ContentModule,
-<<<<<<< HEAD
-    PostsModule,
-=======
     CommentModule,
->>>>>>> 8c9309bff53e00f38c7bc1e80f84ccbb19eef52f
   ],
   controllers: [AppController, ],
   providers: [AppService, ],
